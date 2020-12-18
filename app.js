9fc0560356370e238ca0bc53eb0b48f75d3b9986
Kiran Debnath <kirandebnath81@gmail.com>
2020-12-19 01:05:16 +0530

    feat: added features

---
+++ app.js
@@ -0,0 +1,28 @@
+let inputText=document.querySelector(".textarea");
+let btnPrimary=document.querySelector(".btn-primary");
+let outputText=document.querySelector(".output")
+
+btnPrimary.addEventListener("click",eventHandler)
+
+
+// if(inputText.value===""){
+// alert('Enter some text')
+// else{
+    function eventHandler(){
+        let userInput=inputText.value
+    
+        
+        fetch(urlConstructer(userInput))
+        .then(response => response.json())
+        .then(json => {
+            outputText.innerText=json.contents.translated;
+        })
+    }
+    
+    
+    
+     var url="https://api.funtranslations.com/translate/minion.json"
+    
+    function urlConstructer(text){
+        return url+"?text="+text
+    }
